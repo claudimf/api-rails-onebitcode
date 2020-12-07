4aa1247e856dadd4d72694320fd0bee9eba9bcd0
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2020-12-07 13:37:01 -0300

    Sets up Rack Attack, Devise and Simple token

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,6 @@ class Rack::Attack
  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
  # Allow all local traffic
-
  safelist('allow-localhost') do |req|
    '127.0.0.1' == req.ip || '::1' == req.ip
  end
